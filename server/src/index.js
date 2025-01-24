52a2bece786b1543f3aae11196f5e8a68529d76f
Sukriti Sharma <153065339+sukkuzzz@users.noreply.github.com>
2025-01-24 16:52:31 +0530

    Update index.js Deployemnt part

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URI
+  `https://whitecarrotassignment-backend.onrender.com/auth/redirect`,
 );
 
 oauth2Client.on("tokens", (tokens) => {
